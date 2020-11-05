0b3d6331bf8c02477090dd3e479db61aadf971e8
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-05 04:06:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ end
 end
 
 def list_dwarves(array)
-  list_dwarves.each.with_index(1)  do |dwarf, index|
-    puts "#{index}. #{dwarf}"
+  dwarves.each_with_index do |name, index|
+     puts "#{index+1}. #{name}"
   end
 end
   # Use `each_with_index` to enumerate over the provided array
